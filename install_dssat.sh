ed00704e55e8d5ba62eb9f24664b61ef54fed207
Jens Krumsieck <j.krumsieck@outlook.de>
2024-07-19 13:45:36 +0000

    make installation folder variable

--- install_dssat.sh
+++ install_dssat.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
-git clone https://github.com/DSSAT/dssat-csm-os ~/dssat-csm-os --branch v4.8.0.27 && mkdir ~/dssat-csm-os/build && cd ~/dssat-csm-os/build && cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=~/dssat && make
+dir = ~/dssat
+if [ ! $1 == "" ]; then
+dir = $1
+fi
+echo $dir
+
+git clone https://github.com/DSSAT/dssat-csm-os ~/dssat-csm-os --branch v4.8.0.27 && mkdir ~/dssat-csm-os/build && cd ~/dssat-csm-os/build && cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=$dir && make
 git clone https://github.com/DSSAT/dssat-csm-data ~/dssat-csm-data --branch v4.8.0.27 && cp -r ~/dssat-csm-data/* ~/dssat-csm-os/Data
 mkdir ~/dssat/
-cp -r ~/dssat-csm-os/Data/* ~/dssat/
-cp -r ~/dssat-csm-os/build/bin/* ~/dssat/
+cp -r ~/dssat-csm-os/Data/* $dir/
+cp -r ~/dssat-csm-os/build/bin/* $dir/
