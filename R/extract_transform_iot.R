6ae2b961334b5a2ce9a8ae75964b9fa96f5a4af4
Benjamin <benjamin.leroy13@gmail.com>
2024-09-26 19:08:25 +0200

    simplify get and format funs

--- R/extract_transform_iot.R
+++ R/extract_transform_iot.R
@@ -4,11 +4,17 @@ library(httr)
 library(jsonlite)
 library(lubridate)
 library(dplyr)
+library(purrr)
+library(units)
+library(DSSAT)
+library(usethis)
+
+#use_build_ignore("./R/extract_transform_iot.R")
 
 
-# Obtain token (4min validity) --------------------------------------------
+# Obtain keycloak token (4min validity) --------------------------------------------
 
-get_frost_token <- function(url, client_id, client_secret, username, password) {
+get_kc_token <- function(url, client_id, client_secret, username, password) {
   
   response <- POST(
     url = url,
@@ -28,8 +34,6 @@ get_frost_token <- function(url, client_id, client_secret, username, password) {
   
   return(token)
 }
-
-token <- get_frost_token(url = base_url, client_id = client_id, client_secret = client_secret, username = username, password = password)
 
 
 # NOT RUN: simulate and push data onto FROST server --------------------------
@@ -200,132 +204,125 @@ token <- get_frost_token(url = base_url, client_id = client_id, client_secret = 
 # TODO: silence messages
 
 
-# Get observations and metadata ------------------------------------------- TODO
+#### Get obseverations and metadata -----------
 
 # Get sensor metadata
-token <- get_frost_token(url = base_url, client_id = client_id,
-                         client_secret = client_secret, username = username, password = password)
-
-
-get_iot_metadata <- function(url, token,
-                             object = c("device","sensor","observed_property","datastream","observations"),
-                             object_id) {
+get_ogc_obs <- function(url_user, datastream_id, token) {
   
-  url <- paste0(url, switch(object,
-                            "device" = paste0("Things(", object_id, ")"),
-                            "sensor" = paste0("Sensors(", object_id, ")"),
-                            "observed_property" = paste0("ObservedProperties(", object_id, ")"),
-                            "datastream" = paste0("Datastream(", object_id, ")")
-  ))
+  # Build URL: currently select phenomenonTime and resul
+  # See: https://developers.sensorup.com/docs/#observations_get for additional attributes
+  url <- paste0(url_user, "Datastreams(", datastream_id, ")/Observations?$select=phenomenonTime,result")
+  observations <- data.frame(phenomenonTime = character(), result = character(), stringsAsFactors = FALSE)
   
-  raw <- GET(url, add_headers(`Authorization` = paste("Bearer", token)))
-  
-  json <- fromJSON(content(raw, as = "text"))
-  return(as.data.frame(json))
-}
-
-tmp <- get_iot_metadata(url = user_url,
-                        token = token,
-                        object = "device",
-                        object_id = 7)
-
-
-airtemp_sensor_raw <- GET(paste0(url, "Sensors(1)"), 
-                      add_headers(`Authorization` = paste("Bearer", token)))
-airtemp_sensor_json <- fromJSON(
-  content(airtemp_sensor_raw, as = "text")
-)
-
-# TODO: pull back device/location/sensor/property metadata from datastream
-get_iot_data <- function(url, datastream_id, token) {
-  
-  # function to replace fill null columns
-  replace_null_with_na <- function(x) {
-    if (is.null(x)) {
-      return(NA)
-    } else if (is.list(x)) {
-      return(lapply(x, replace_null_with_na))
-    } else {
-      return(x)
-    }
+  # Get data (time series)
+  while (!is.null(url)) {
+    response <- GET(url, add_headers(`Authorization` = paste("Bearer", token)))
+    data <- fromJSON(
+      content(response, as = "text", encoding = "UTF-8")
+    )
+    observations <- rbind(observations, data$value)
+    url <- data$`@iot.nextLink`
   }
-  
-  # Get data
-  all_obs <- list()
-  i <- 1
-  
-  repeat {
-    url_obs <- paste0(url, "Datastreams(", datastream_id, ")/Observations(", i, ")")
-
-    raw <- GET(url_obs, add_headers(`Authorization` = paste("Bearer", token)))
-    
-    if (status_code(raw) != 200) {
-      break
-    }
-    
-    all_obs[[i]] <- fromJSON(content(raw, as = "text"))
-    all_obs[[i]] <- replace_null_with_na(all_obs[[i]])  # necessary for dataframe conversion
-    i <- i + 1
-  }
-  
-  data <- do.call(rbind, lapply(all_obs, as.data.frame))
   
   
   # Get metadata
-  url_ds <- paste0(user_url, "Datastreams(", datastream_id, ")")
-  metadata_datastream <- fromJSON(
-    content(
-      GET(url_ds, add_headers(`Authorization` = paste("Bearer", token))),
-      as = "text")
-  )
+  get_metadata <- function(url, token) {
+    response <- GET(url, add_headers(`Authorization` = paste("Bearer", token)))
+    fromJSON(content(response, as = "text"))
+  }
   
-  metadata_property <- fromJSON(
-    content(
-      GET(metadata_datastream$`ObservedProperty@iot.navigationLink`,
-          add_headers(`Authorization` = paste("Bearer", token))),
-      as = "text")
-  )
-  
-  metadata_device <- fromJSON(
-    content(
-      GET(metadata_datastream$`Thing@iot.navigationLink`,
-          add_headers(`Authorization` = paste("Bearer", token))),
-      as = "text")
-  )
-  
-  metadata_sensor <- fromJSON(
-    content(
-      GET(metadata_datastream$`Sensor@iot.navigationLink`,
-          add_headers(`Authorization` = paste("Bearer", token))),
-      as = "text")
-  )
+  # Base URL for the Datastream
+  url_datastream <- paste0(url_user, "Datastreams(", datastream_id, ")")
+  datastream <- get_metadata(url_datastream, token)  # measured location (xy), measurement unit (name+symbol)
+  property <- get_metadata(datastream$`ObservedProperty@iot.navigationLink`, token)  # property name (user-defined; here AGROVOC + snake_case)
+  device <- get_metadata(datastream$`Thing@iot.navigationLink`, token)  # name + description device
+  sensor <- get_metadata(datastream$`Sensor@iot.navigationLink`, token)  # name + metadata sensor (measurement heights?)
   
   # Compile metadata
   metadata <- list(
-    device = metadata_device,
-    sensor = metadata_sensor,
-    datasstream = metadata_datastream,
-    property = metadata_property
+    device = device,
+    sensor = sensor,
+    datastream = datastream,
+    property = property
   )
   
-  out <- list(data = data, metadata = metadata)
+  out <- list(data = observations, metadata = metadata)
   
   return(out)
 }
 
-token <- get_frost_token(url = base_url, client_id = client_id,
-                         client_secret = client_secret, username = username, password = password)
-obs_airtemp <- get_iot_data(url = user_url, datastream_id = 1, token = token)
+token <- get_kc_token(url = base_url, client_id = client_id, client_secret = client_secret, username = username, password = password)
+obs_airtemp <- get_ogc_obs(url_user = user_url, datastream_id = 1, token = token)
+
+# Compile DSSAT header
+# TODO: handle original station name/DSSAT station name
+# TODO: add commentary header: data pulled from TUM Sensorhub with csmTools on ####.##.###; Platform-registered device name: ####; sensor name: ####.
+# TODO: format DSSAT output weather file (POST AND ADD MORE VARIABLES: SRAD, RAIN, WIND, RHUM)
+# input_dssat[[1]]$WEAT_METADATA
+format_ogc_icasa <- function(data, metadata){
+}
 
 
-# Transform to daily data
-obs_airtemp_fmt <- 
-  as.data.frame(obs_airtemp$data) %>%
-  mutate(result = as.numeric(result),
-         date = as.Date(ymd_hms(phenomenonTime))) %>%
-  group_by(date) %>%
-  summarise(TMIN = min(result, na.rm = TRUE),
-            TMAX = max(result, na.rm = TRUE),
-            TMEAN = mean(result, na.rm = TRUE), .groups = "drop")
+data <- obs_airtemp$data
+metadata <- obs_airtemp$metadata
+var <- metadata$property$name  # standard: https://agrovoc.fao.org/browse/agrovoc/en/page/c_6496
+units <- make_units(metadata$datastream$unitOfMeasurement$name)  # standard: https://en.wikipedia.org/wiki/List_of_metric_units
 
-# TODO: make metadata header for 
+# Spatial metadata
+lon <- metadata$datastream$observedArea$coordinates[[1]]
+lat <- metadata$datastream$observedArea$coordinates[[2]]
+obj_type <- metadata$datastream$observedArea$type
+
+# Format data to ICASA
+data <- data %>%
+  mutate(!!sym(var) := as.numeric(result),
+         DATE = as.Date(ymd_hms(phenomenonTime))) %>%
+  select(DATE, !!sym(var))
+
+### TODO: GET UNITS AND CONVERT IF NEEDED
+
+# TODO: define ofc(agrovoc)-icasa data map as internal data
+agg_funs <- list(
+  TMAX = ~ if (cur_column() == "air_temperature") { max(.x, na.rm = TRUE) } else { NA} ,
+  TMIN = ~ if (cur_column() == "air_temperature") { min(.x, na.rm = TRUE) } else { NA },
+  RAIN = ~ if (cur_column() == "rainfall") { sum(.x, na.rm = TRUE) } else { NA },
+  DEWP = ~ if (cur_column() == "dew_point_temperature") { mean(.x, na.rm = TRUE) } else { NA },
+  WIND = ~ if (cur_column() == "wind_speed") { sum(.x, na.rm = TRUE) } else { NA },
+  RHUM = ~ if (cur_column() == "relative_humidity") { mean(.x, na.rm = TRUE) } else { NA }
+)
+
+data_daily <- data %>%
+  group_by(DATE) %>%
+  summarise(across(all_of(var), agg_funs, .names = "{.fn}"))
+
+# Format mmetadata
+tav <- data_daily %>%
+  summarise(TAV = mean((TMAX + TMIN)/2, na.rm = TRUE)) %>%
+  pull(TAV)
+amp <- data_daily %>%
+  mutate(mo = month(DATE)) %>%
+  group_by(mo) %>%
+  summarise(mo_TAV = mean((TMAX + TMIN)/2, na.rm = TRUE)) %>%
+  summarise(AMP = (max(mo_TAV)-min(mo_TAV))/2) %>%
+  pull(AMP)
+
+metadata <- data.frame(
+  WSTA = "DEFS",  # TODO: retrieve location name and produce name
+  LAT = lat,
+  LONG = lon,
+  ELEV = NA,
+  TAV = tav,
+  AMP = amp,
+  REFHT = 2,  # document in device/sensor metadata? Would need text mining workflow
+  WNDHT = NA  # document in device/sensor metadata?
+  )
+
+comments <- "TODO"
+
+# TODO: add_property_mapping <- function(name, unit, icasa = list(), agg_funs = list())
+
+
+
+###---- Compile file--------------------
+iot_airtemp_fmt <- build_wth(ls = iot_airtemp, keep_unmapped = FALSE) # TODO: Test with SITE and DESCRIPTION in template
+write_wth2(iot_airtemp_fmt, "test_iot.WTH")  # TODO: correct LAT/LONG
